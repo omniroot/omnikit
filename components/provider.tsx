0379161cb5d700438f411cf1c3a7000a559517c3
omniroot <omnirootofc@gmail.com>
2025-10-01 13:27:39 +0300

    Add toaster in provider

--- components/provider.tsx
+++ components/provider.tsx
@@ -5,20 +5,19 @@ import { useState } from "react";
 import { themes } from "@/theme";
 import { ColorModeProvider, type ColorModeProviderProps } from "./color-mode";
 import "./provider.css";
+import { Toaster } from "@/theme/components/toaster.tsx";
 
 interface IOmnikitProvider extends ColorModeProviderProps {
 	theme?: "linear" | "coffee";
 }
 
-export function OmnikitProvider({
-	theme = "linear",
-	...props
-}: IOmnikitProvider) {
+export function OmnikitProvider({ theme = "linear", ...props }: IOmnikitProvider) {
 	const [selectedTheme] = useState(theme);
 
 	return (
 		<ChakraProvider value={themes[selectedTheme]}>
 			<ColorModeProvider {...props} />
+			<Toaster />
 		</ChakraProvider>
 	);
 }
