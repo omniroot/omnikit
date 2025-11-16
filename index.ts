fd81ad8fea21d52b5ca67479642cbd5d4a5f2275
omniroot <omnirootofc@gmail.com>
2025-11-16 10:50:38 +0300

    Remove coffee theme

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
-import coffeeTheme from "@/theme/coffee/index.ts";
+// import coffeeTheme from "@/theme/coffee/index.ts";
 import linearTheme from "@/theme/linear/index.ts";
 
 export const themes = {
 	linear: linearTheme,
-	coffee: coffeeTheme,
+	// coffee: coffeeTheme,
 	// material: materialTheme,
 	// liquidGlass: liquidGlassTheme,
 };
