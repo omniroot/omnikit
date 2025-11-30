4c5ba87963e78169ab3f05599342a0ef9c5c6877
omniroot <omnirootofc@gmail.com>
2025-11-30 06:49:46 +0300

    Add initialEmoji and onlyPreview in EmojiPicker

--- components/EmojiPicker.tsx
+++ components/EmojiPicker.tsx
@@ -1,8 +1,9 @@
-import { IconButton, Popover, Portal, useDisclosure } from "@chakra-ui/react";
+import { Box, IconButton, Popover, Portal, useDisclosure } from "@chakra-ui/react";
 import data from "@emoji-mart/data";
 import Picker from "@emoji-mart/react";
 import { IconMoodUnamused } from "@tabler/icons-react";
-import { type FC, useState } from "react";
+import { getEmojiDataFromNative } from "emoji-mart";
+import { type FC, useEffect, useState } from "react";
 
 // import "./EmojiPicker.css";
 
@@ -15,10 +16,28 @@ export interface IEmoji {
 	unified: string;
 }
 
+const getEmojiById = (id: IEmoji["id"]): IEmoji | undefined => {
+	// @ts-expect-error
+	console.log("emo", data?.emojis);
+	const result = data?.emojis?.[id];
+
+	console.log("Emo result", result);
+
+	return result;
+};
+
+console.log("@Emoji ", { data });
+
 interface IProps {
+	initialEmoji?: IEmoji["native"];
 	onChange?: (emoji: IEmoji) => void;
+	onlyPreview?: boolean;
 }
-export const EmojiPicker: FC<IProps> = ({ onChange }) => {
+export const EmojiPicker: FC<IProps> = ({
+	initialEmoji,
+	onChange,
+	onlyPreview = false,
+}) => {
 	const [emoji, setEmoji] = useState<IEmoji | null>(null);
 	const { open, onToggle } = useDisclosure();
 	// const [search, setSearch] = useState("Hearth");
@@ -28,7 +47,24 @@ export const EmojiPicker: FC<IProps> = ({ onChange }) => {
 		onChange?.(emoji);
 	};
 
-	// console.log({ emoji });
+	useEffect(() => {
+		const setupInitialEmoji = async () => {
+			const emoji = (await getEmojiDataFromNative(initialEmoji)) as IEmoji;
+			if (emoji) setEmoji(emoji);
+		};
+		if (initialEmoji) {
+			setupInitialEmoji();
+		}
+	}, [initialEmoji]);
+
+	console.log({ emoji });
+
+	if (onlyPreview)
+		return (
+			<Box p={2} fontSize={"md"}>
+				{emoji?.native || <IconMoodUnamused />}
+			</Box>
+		);
 
 	return (
 		<Popover.Root open={open} onOpenChange={onToggle} positioning={{ strategy: "fixed" }}>
