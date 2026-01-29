171ae9997be1006b36c82505669ae36b4794939e
omniroot <omnirootofc@gmail.com>
2026-01-29 21:46:11 +0300

    fix: fix font loading

--- kaizen/tokens/fonts.ts
+++ kaizen/tokens/fonts.ts
@@ -3,14 +3,14 @@ import { defineTokens } from "@chakra-ui/react";
 export const fonts = defineTokens.fonts({
 	heading: {
 		value:
-			'Inter, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+			'Kaizen-Heading, Inter, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
 	},
 	body: {
 		value:
-			'Inter, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+			'Kaizen-Body, Inter, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
 	},
 	mono: {
 		value:
-			'SFMono-Regular,Menlo,Monaco,Consolas,"Liberation Mono","Courier New",monospace',
+			'Kaizen-Body, SFMono-Regular,Menlo,Monaco,Consolas,"Liberation Mono","Courier New",monospace',
 	},
 });
